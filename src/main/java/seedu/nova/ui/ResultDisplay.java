e3a8089893b34f3dc080628226ad52353f682132
Loh Sze Ying <bloom9334@hotmail.com>
2020-04-01 22:44:11 +0800

    Changed to duke-gui like

--- src/main/java/seedu/nova/ui/ResultDisplay.java
+++ src/main/java/seedu/nova/ui/ResultDisplay.java
@@ -2,9 +2,12 @@ package seedu.nova.ui;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
+import javafx.scene.text.TextAlignment;
 
 /**
  * A ui for the status bar that is displayed at the header of the application.
@@ -14,7 +17,7 @@ public class ResultDisplay extends UiPart<Region> {
     private static final String FXML = "ResultDisplay.fxml";
 
     @FXML
-    private TextArea resultDisplay;
+    private Label resultDisplay;
 
     public ResultDisplay() {
         super(FXML);
@@ -22,6 +25,8 @@ public class ResultDisplay extends UiPart<Region> {
 
     public void setFeedbackToUser(String feedbackToUser) {
         requireNonNull(feedbackToUser);
+        //System.out.println(property.getValue());
+        //resultDisplay.setPrefWidth(property.getValue());
         resultDisplay.setText(feedbackToUser);
     }
 
